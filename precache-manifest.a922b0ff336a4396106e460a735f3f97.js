182ee1d49920061eebd2fb0369be8cf232a9af0a
Daniel Schroeder <dschroeder101@gmail.com>
2020-06-02 16:21:41 -0700

    Updates

---
+++ precache-manifest.a922b0ff336a4396106e460a735f3f97.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a2830f7a2b5cc43c7b9ec0ca0752b4",
+    "url": "/%7Bunholy-karaoke%7D/index.html"
+  },
+  {
+    "revision": "1b7064be2cbeefcd4e85",
+    "url": "/%7Bunholy-karaoke%7D/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a19ac37691af6ec3b61f",
+    "url": "/%7Bunholy-karaoke%7D/static/css/main.50c647c7.chunk.css"
+  },
+  {
+    "revision": "1b7064be2cbeefcd4e85",
+    "url": "/%7Bunholy-karaoke%7D/static/js/2.0580f526.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/%7Bunholy-karaoke%7D/static/js/2.0580f526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19ac37691af6ec3b61f",
+    "url": "/%7Bunholy-karaoke%7D/static/js/main.e79cd0ab.chunk.js"
+  },
+  {
+    "revision": "7f25b1d4fa4063a48cf9",
+    "url": "/%7Bunholy-karaoke%7D/static/js/runtime-main.8b1d0649.js"
+  },
+  {
+    "revision": "65e13358537bf38242b97b4456332a06",
+    "url": "/%7Bunholy-karaoke%7D/static/media/bark.65e13358.ogg"
+  },
+  {
+    "revision": "db87ef5779b15c66191e1d00cbfa877c",
+    "url": "/%7Bunholy-karaoke%7D/static/media/drip.db87ef57.ogg"
+  },
+  {
+    "revision": "3f1c966cf2add53e0a87168394b90e20",
+    "url": "/%7Bunholy-karaoke%7D/static/media/glass.3f1c966c.ogg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7Bunholy-karaoke%7D/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e4b6d592f3fc8b281ac93ceb0e7b3b3a",
+    "url": "/%7Bunholy-karaoke%7D/static/media/sonar.e4b6d592.ogg"
+  }
+]);
